8a330890e029aceaf3fb50b14d3ae4acbe72e1fa
Guna Vardhan <varivardhan15@gmail.com>
2025-12-30 15:47:32 +0530

    auth-server file fixed

--- lib/auth-server.ts
+++ lib/auth-server.ts
@@ -1,6 +1,10 @@
 import { createAuth } from "@/convex/auth";
 import { getToken as getTokenNextjs } from "@convex-dev/better-auth/nextjs";
+import { cookies } from "next/headers";
+
 
 export const getToken = () => {
+    // 🔥 This line is CRITICAL
+    cookies();
     return getTokenNextjs(createAuth);
 };
